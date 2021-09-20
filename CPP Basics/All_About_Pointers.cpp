95273faedff033d0b17485b8386e95727169dee6
Vyom-Yadav <jackhammervyom@gmail.com>
2021-09-20 15:50:39 +0530

    Modified gitignore and Pointer Logic a bit

--- CPP Basics/All_About_Pointers.cpp
+++ CPP Basics/All_About_Pointers.cpp
@@ -9,13 +9,15 @@ int main(){
     *ptr = 20;
     cout<<a<<" "<<*ptr<<endl; */
     // Double pointer
-    int **ptr1 = &ptr;
+    int **ptr1; // Dont relate star logic from here, it used when we apply after declaration.
+    ptr1 = &ptr;
     cout<<"The value of a: "<<a<<endl;
     cout<<"The value of a inside pointer: "<<*ptr<<endl;
     cout<<"The address of a: "<<&a<<endl;
     cout<<"The address of a stored in pointer: "<<ptr<<endl;
     cout<<"The address of pointer: "<<&ptr<<endl;
     cout<<"The value in double pointer: "<<ptr1<<endl;
+    cout<<"The value stored in double pointer can also be called like: "<<*(&ptr1)<<endl;
     // The most simple logic I can give you is-
     // '&'- Gives the address of a varible.( It is called as the refrence variable, it gives the refrence ). Read it as address.
     // '*' - Gives the value stored at that address. As simple as that.
